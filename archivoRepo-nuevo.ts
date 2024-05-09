2b475c51a1d01b4bf5bf3312425b19197fc8c414
Diego Fischer <diegof01gm@gmail.com>
2024-05-08 21:32:20 -0300

    segundo mensaje

--- archivoRepo-nuevo.ts
+++ archivoRepo-nuevo.ts
@@ -1,1 +1,5 @@
 console.log("Hola nuevo archivo");
+console.log ("Segundo add y commit");
+
+//git add ( y nombre del archivo sin comillas)
+//git commit -m "y el mensaje de los cambio entre comillas"
